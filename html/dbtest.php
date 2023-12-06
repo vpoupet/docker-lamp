5b0e0fb1a451e9a46c53be7efe41d99f733508e3
Victor Poupet <37943580+vpoupet@users.noreply.github.com>
2023-12-06 09:47:36 +0100

    Update dbtest.php

--- html/dbtest.php
+++ html/dbtest.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "mariadb";  // nom du serveur MariaDB (ici le nom du container Docker)
 $username = "root";       // nom d'utilisateur
-$password = "azpoazui";      // mot de passe (le mot de passe de root est défini dans le fichier docker-compose.yml)
+$password = "admin";      // mot de passe (le mot de passe de root est défini dans le fichier docker-compose.yml)
 
 // Ouvrir une connexion
 $conn = new mysqli($servername, $username, $password);
